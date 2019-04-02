262f3a572bbe372b0bd77deca379404f6f1d35d5
Yizhen Shi <yizhen1001@gmail.com>
2019-04-02 17:44:57 -0400

    ready to deploy

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -2,21 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../interface/todo';
 import { trigger, transition, style, animate } from '@angular/animations'
 @Component({
-// tslint:disable-next-line: component-selector
+	// tslint:disable-next-line: component-selector
 	selector: 'todo-list',
 	templateUrl: './todo-list.component.html',
 	styleUrls: ['./todo-list.component.css'],
-	animations:[
-		trigger('fade',[
+	animations: [
+		trigger('fade', [
 
-			transition(':enter',[
-				style({opacity:0,transform:'translateY(30px)'}),
-				animate(200,style({opacity:1,transform:'translateY(0px)'}))
+			transition(':enter', [
+				style({ opacity: 0, transform: 'translateY(30px)' }),
+				animate(200, style({ opacity: 1, transform: 'translateY(0px)' }))
 			]),
-			transition(':leave',[
-				
-				animate(200,style({opacity:0,transform:'translateY(30px)'})),
-				
+			transition(':leave', [
+
+				animate(200, style({ opacity: 0, transform: 'translateY(30px)' })),
+
 			])
 
 		])
@@ -24,8 +24,8 @@ import { trigger, transition, style, animate } from '@angular/animations'
 })
 export class TodoListComponent implements OnInit {
 	public todos: object[];
-	todoTitle:string;
-	idForTodo:number;
+	todoTitle: string;
+	idForTodo: number;
 	beforeEditCache: string;
 	filter: string;
 
@@ -35,56 +35,68 @@ export class TodoListComponent implements OnInit {
 	ngOnInit() {
 		this.filter = 'all';
 		this.beforeEditCache = '';
-		this.idForTodo = 2;
+		this.idForTodo = 4;
 		this.todoTitle = '';
 		this.todos = [
 
 			{
 				id: 1,
-				title: 'Finish Angular app',
+				title: 'This is an Angular To-do list app',
+				completed: false,
+				editing: false,
+			},
+			{
+				id: 2,
+				title: 'You can double click to edit an item',
+				completed: false,
+				editing: false,
+			},
+			{
+				id: 3,
+				title: 'All right reserved by Yizhen Shi',
 				completed: false,
 				editing: false,
 			}
 		]
 	}
 
-	addTodo(): void{
-		if(this.todoTitle.trim().length === 0){
+	addTodo(): void {
+		if (this.todoTitle.trim().length === 0) {
 			return;
 		}
 		this.todos.push({
-			id : this.idForTodo,
-			title : this.todoTitle,
+			id: this.idForTodo,
+			title: this.todoTitle,
 			completed: false,
 			editing: false,
 		})
-	
-	this.todoTitle = '';
-	this.idForTodo++;
+
+		this.todoTitle = '';
+		this.idForTodo++;
 	}
 
-	editTodo(todo: Todo):void{
+	editTodo(todo: Todo): void {
 		this.beforeEditCache = todo.title;
 		todo.editing = true;
 	}
 
-	doneEdit(todo: Todo):void {
-		if(todo.title.trim().length === 0){
+	doneEdit(todo: Todo): void {
+		if (todo.title.trim().length === 0) {
 			todo.title = this.beforeEditCache;
 		}
 		todo.editing = false;
 	}
 
-	cancelEdit(todo: Todo):void {
+	cancelEdit(todo: Todo): void {
 		todo.title = this.beforeEditCache;
 		todo.editing = false;
 	}
 
-	deleteTodo(id):void{
+	deleteTodo(id): void {
 		this.todos = this.todos.filter(todo => todo.id !== id);
 	}
 
-	remaining():number {
+	remaining(): number {
 		return this.todos.filter(todo => !todo.completed).length;
 	}
 
@@ -93,20 +105,20 @@ export class TodoListComponent implements OnInit {
 		return this.todos.filter(todo => todo.completed).length > 0;
 	}
 
-	clearCompleted():void {
+	clearCompleted(): void {
 		this.todos = this.todos.filter(todo => !todo.completed);
 	}
 
-	checkAllTodos(): void{
+	checkAllTodos(): void {
 		this.todos.forEach(todo => todo.completed = (event.target as HTMLInputElement).checked);
 	}
 
 	todosFiltered(): Todo[] {
-		if(this.filter === 'all') {
+		if (this.filter === 'all') {
 			return this.todos;
-		}else if(this.filter === 'active'){
+		} else if (this.filter === 'active') {
 			return this.todos.filter(todo => !todo.completed);
-		}else if(this.filter === 'completed'){
+		} else if (this.filter === 'completed') {
 			return this.todos.filter(todo => todo.completed)
 		}
 
